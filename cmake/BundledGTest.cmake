b498e0b4b1c0f35272e2d6ef9347b33556bd3c4c
Nihar Sawant <NIharswaantrockstar2013@gamil.com>
2025-11-13 00:37:13 +0100

    revert

--- cmake/BundledGTest.cmake
+++ cmake/BundledGTest.cmake
@@ -1,18 +1,11 @@
 find_package(GTest)
 
-if(NOT GTEST_FOUND)
-    message(STATUS "Adding bundled Google Test")
+if (NOT GTEST_FOUND)
+   message(STATUS "Adding bundled Google Test")
+   set(BUILD_GMOCK OFF CACHE BOOL INTERNAL)
+   set(INSTALL_GTEST OFF CACHE BOOL INTERNAL)
 
-    # Turn off building GMock and installing GTest
-    set(BUILD_GMOCK OFF CACHE BOOL INTERNAL)
-    set(INSTALL_GTEST OFF CACHE BOOL INTERNAL)
+   add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/thirdparty/googletest)
 
-    # Explicit binary directory required when source is outside current project
-    set(GTEST_SRC_DIR "${CMAKE_CURRENT_LIST_DIR}/thirdparty/googletest")
-    set(GTEST_BIN_DIR "${CMAKE_BINARY_DIR}/thirdparty/googletest_build")
-
-    add_subdirectory("${GTEST_SRC_DIR}" "${GTEST_BIN_DIR}")
-
-    add_library(GTest::GTest ALIAS gtest)
-    add_library(GTest::Main ALIAS gtest_main)
-endif()
+   add_library(GTest::GTest ALIAS gtest)
+endif ()
