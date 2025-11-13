8a2d6ecafd75b3dc539fca56e4eaa398ff6ae0f0
Nihar Sawant <NIharswaantrockstar2013@gamil.com>
2025-11-13 11:34:10 +0100

    test

--- cmake/BundledGTest.cmake
+++ cmake/BundledGTest.cmake
@@ -1,11 +1,29 @@
 find_package(GTest)
 
-if (NOT GTEST_FOUND)
-   message(STATUS "Adding bundled Google Test")
-   set(BUILD_GMOCK OFF CACHE BOOL INTERNAL)
-   set(INSTALL_GTEST OFF CACHE BOOL INTERNAL)
+if (NOT GTest_FOUND)
+    message(STATUS "Adding bundled Google Test")
 
-   add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/thirdparty/googletest)
+    set(GTEST_SRC_DIR "${CMAKE_CURRENT_LIST_DIR}/thirdparty/googletest")
 
-   add_library(GTest::GTest ALIAS gtest)
-endif ()
+    if (EXISTS "${GTEST_SRC_DIR}/CMakeLists.txt")
+        set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
+        set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
+
+        # Specify a binary directory explicitly (required for out-of-tree builds)
+        set(GTEST_BIN_DIR "${CMAKE_BINARY_DIR}/_gtest_build")
+        add_subdirectory("${GTEST_SRC_DIR}" "${GTEST_BIN_DIR}")
+
+        # Ensure alias targets exist regardless of upstream naming
+        if (TARGET gtest)
+            add_library(GTest::GTest ALIAS gtest)
+        elseif (TARGET GTest::gtest)
+            add_library(GTest::GTest ALIAS GTest::gtest)
+        elseif (TARGET gtest_main)
+            add_library(GTest::GTest ALIAS gtest_main)
+        else()
+            message(WARNING "No valid gtest target found after building bundled GoogleTest.")
+        endif()
+    else()
+        message(FATAL_ERROR "Bundled Google Test source not found at ${GTEST_SRC_DIR}")
+    endif()
+endif()
