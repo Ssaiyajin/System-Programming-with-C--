2d24025e9c082613fb007ed46a358c5ff5827ce7
Nihar Sawant <NIharswaantrockstar2013@gamil.com>
2025-11-13 23:41:59 +0100

    test

--- pooled_list_allocator/lib/MallocAllocator.hpp
+++ pooled_list_allocator/lib/MallocAllocator.hpp
@@ -8,30 +8,46 @@
 
 namespace pool {
 
-template<typename T>
-struct MallocAllocator {
+template <typename T>
+class MallocAllocator {
+public:
     using value_type = T;
 
-    MallocAllocator() noexcept = default;
-    MallocAllocator(const MallocAllocator&) noexcept = default;
-    MallocAllocator(MallocAllocator&&) noexcept = default;
-    MallocAllocator& operator=(const MallocAllocator&) noexcept = default;
-    MallocAllocator& operator=(MallocAllocator&&) noexcept = default;
+    // Mandatory typedefs
+    using pointer = T*;
+    using const_pointer = const T*;
+    using reference = T&;
+    using const_reference = const T&;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
 
-    template<typename U>
-    struct rebind { using other = MallocAllocator<U>; };
+    // Required by STL allocator model
+    template <typename U>
+    struct rebind {
+        using other = MallocAllocator<U>;
+    };
 
-    T* allocate() {
-        void* p = std::malloc(sizeof(T));
-        if (!p) throw std::bad_alloc();
-        return static_cast<T*>(p);
+    MallocAllocator() noexcept {}
+    template <typename U>
+    MallocAllocator(const MallocAllocator<U>&) noexcept {}
+
+    pointer allocate(size_type n) {
+        if (n == 0)
+            return nullptr;
+
+        if (n > static_cast<size_type>(-1) / sizeof(T))
+            throw std::bad_alloc();
+
+        void* p = std::malloc(n * sizeof(T));
+        if (!p)
+            throw std::bad_alloc();
+        return static_cast<pointer>(p);
     }
 
-    void deallocate(T* p) noexcept {
-        std::free(static_cast<void*>(p));
+    void deallocate(pointer p, size_type) noexcept {
+        std::free(p);
     }
 };
-
 } // namespace pool
 
 #endif
