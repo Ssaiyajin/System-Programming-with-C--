7905d3d56c74e51050c99713567cd79c6f76fafc
Nihar Sawant <NIharswaantrockstar2013@gamil.com>
2025-11-13 11:19:25 +0100

    test

--- cmake/BundledGTest.cmake
+++ cmake/BundledGTest.cmake
@@ -6,7 +6,11 @@ if (NOT benchmark_FOUND)
     set(BENCHMARK_SRC_DIR "${CMAKE_CURRENT_LIST_DIR}/thirdparty/benchmark")
 
     if (EXISTS "${BENCHMARK_SRC_DIR}/CMakeLists.txt")
-        # Use explicit binary dir when called out-of-tree
+        # Disable building Benchmark’s internal tests (which depend on GTest)
+        set(BENCHMARK_ENABLE_GTEST_TESTS OFF CACHE BOOL "Disable GTest in Benchmark" FORCE)
+        set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "Disable internal testing" FORCE)
+
+        # Use explicit binary directory for out-of-tree builds
         set(BENCHMARK_BIN_DIR "${CMAKE_BINARY_DIR}/_benchmark_build")
         add_subdirectory("${BENCHMARK_SRC_DIR}" "${BENCHMARK_BIN_DIR}")
 
