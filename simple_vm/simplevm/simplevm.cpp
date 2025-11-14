7a27f9b2fc317aa60a94db170efe059233e5869c
Nihar Sawant <NIharswaantrockstar2013@gamil.com>
2025-11-14 15:08:16 +0100

    revert

--- simple_vm/simplevm/simplevm.cpp
+++ simple_vm/simplevm/simplevm.cpp
@@ -2,60 +2,60 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
-#include <stdexcept>
 
 namespace simplevm {
 
-int32_t runVM(const std::vector<std::string>& instructions)
+int32_t runVM()
 {
     int A = 0, B = 0, C = 0, D = 0;
 
-    auto getRegister = [&](char r) -> int& {
-        switch (r) {
-            case 'A': return A;
-            case 'B': return B;
-            case 'C': return C;
-            case 'D': return D;
-            default: throw std::runtime_error("Invalid register");
-        }
-    };
+    std::vector<std::string> instructions;
 
     for (const std::string& instruction : instructions) {
         std::istringstream iss(instruction);
         int opcode;
         iss >> opcode;
 
-        if (opcode == 10) { // load immediate
+        if (opcode == 10) {
             char reg;
-            int value;
-            iss >> reg >> value;
-            getRegister(reg) = value;
-        } 
-        else if (opcode == 20) { // move register
+            int iimm;
+            iss >> reg >> iimm;
+            if (reg == 'A') A = iimm;
+            else if (reg == 'B') B = iimm;
+            else if (reg == 'C') C = iimm;
+            else if (reg == 'D') D = iimm;
+        } else if (opcode == 20) {
             char dest, src;
             iss >> dest >> src;
-            getRegister(dest) = getRegister(src);
-        } 
-        else if (opcode == 50) { // add: C = A + B
-            C = A + B;
-        }
-        else if (opcode == 51) { // sub: C = A - B
-            C = A - B;
-        }
-        else if (opcode == 52) { // rsub: C = B - A
-            C = B - A;
-        }
-        else if (opcode == 53) { // mul: C = A * B
-            C = A * B;
-        }
-        else if (opcode == 54) { // div: C = A / B
-            if (B == 0) {
-                std::cerr << "division by 0" << std::endl;
-                return A;
+            if (dest == 'A') {
+                if (src == 'A') A = A;
+                else if (src == 'B') A = B;
+                else if (src == 'C') A = C;
+                else if (src == 'D') A = D;
+            } else if (dest == 'B') {
+                if (src == 'A') B = A;
+                else if (src == 'B') B = B;
+                else if (src == 'C') B = C;
+                else if (src == 'D') B = D;
+            } else if (dest == 'C') {
+                if (src == 'A') C = A;
+                else if (src == 'B') C = B;
+                else if (src == 'C') C = C;
+                else if (src == 'D') C = D;
+            } else if (dest == 'D') {
+                if (src == 'A') D = A;
+                else if (src == 'B') D = B;
+                else if (src == 'C') D = C;
+                else if (src == 'D') D = D;
             }
-            C = A / B;
-        }
-        else if (opcode == 63) { // exit on error
+        } else if (opcode == 50) {
+            char reg;
+            iss >> reg;
+            if (reg == 'A') A += 1;
+            else if (reg == 'B') B += 1;
+            else if (reg == 'C') C += 1;
+            else if (reg == 'D') D += 1;
+        } else if (opcode == 63) {
             std::cerr << "division by 0" << std::endl;
             return A;
         }
@@ -64,21 +64,23 @@ int32_t runVM(const std::vector<std::string>& instructions)
     return A;
 }
 
-std::vector<std::string> fibonacciProgram(unsigned n)
+void fibonacciProgram(unsigned n)
 {
     std::vector<std::string> program;
-    program.push_back("10 A 0"); // f0
-    program.push_back("10 B 1"); // f1
-    program.push_back("10 C 0"); // fn
+    program.push_back("10A0"); // Set f0 to 0
+    program.push_back("10B1"); // Set f1 to 1
+    program.push_back("10C0"); // Set fn to 0
 
     for (unsigned i = 2; i <= n; i++) {
-        program.push_back("20 D A"); // D = A (fn-2)
-        program.push_back("20 A B"); // A = B (fn-1)
-        program.push_back("20 B C"); // B = C (fn)
-        program.push_back("50 C");   // C = A + B (approx using 50 as placeholder)
+        program.push_back("20D A"); // D = A (fn-2)
+        program.push_back("20A B"); // A = B (fn-1)
+        program.push_back("20B C"); // B = C (fn)
+        program.push_back("50C");   // C = A + 1 (fn+1)
     }
 
-    return program;
+    for (const std::string& instruction : program) {
+        std::cout << instruction << std::endl;
+    }
 }
 
 } // namespace simplevm
