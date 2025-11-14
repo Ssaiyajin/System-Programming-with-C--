980d84c7aa647e317f9a404cca1dab98126d807c
Nihar Sawant <NIharswaantrockstar2013@gamil.com>
2025-11-14 14:45:58 +0100

    test

--- simple_vm/simplevm/simplevm.cpp
+++ simple_vm/simplevm/simplevm.cpp
@@ -9,53 +9,40 @@ int32_t runVM()
 {
     int A = 0, B = 0, C = 0, D = 0;
 
-    std::vector<std::string> instructions;
+    auto getRegister = [&](char r) -> int& {
+        switch (r) {
+            case 'A': return A;
+            case 'B': return B;
+            case 'C': return C;
+            case 'D': return D;
+            default: throw std::runtime_error("Invalid register");
+        }
+    };
+
+    std::vector<std::string> instructions; // your instructions here
 
     for (const std::string& instruction : instructions) {
         std::istringstream iss(instruction);
         int opcode;
         iss >> opcode;
 
-        if (opcode == 10) {
+        if (opcode == 10) { // load immediate
             char reg;
-            int iimm;
-            iss >> reg >> iimm;
-            if (reg == 'A') A = iimm;
-            else if (reg == 'B') B = iimm;
-            else if (reg == 'C') C = iimm;
-            else if (reg == 'D') D = iimm;
-        } else if (opcode == 20) {
+            int value;
+            iss >> reg >> value;
+            getRegister(reg) = value;
+        } 
+        else if (opcode == 20) { // move register to register
             char dest, src;
             iss >> dest >> src;
-            if (dest == 'A') {
-                if (src == 'A') A = A;
-                else if (src == 'B') A = B;
-                else if (src == 'C') A = C;
-                else if (src == 'D') A = D;
-            } else if (dest == 'B') {
-                if (src == 'A') B = A;
-                else if (src == 'B') B = B;
-                else if (src == 'C') B = C;
-                else if (src == 'D') B = D;
-            } else if (dest == 'C') {
-                if (src == 'A') C = A;
-                else if (src == 'B') C = B;
-                else if (src == 'C') C = C;
-                else if (src == 'D') C = D;
-            } else if (dest == 'D') {
-                if (src == 'A') D = A;
-                else if (src == 'B') D = B;
-                else if (src == 'C') D = C;
-                else if (src == 'D') D = D;
-            }
-        } else if (opcode == 50) {
+            getRegister(dest) = getRegister(src);
+        } 
+        else if (opcode == 50) { // increment
             char reg;
             iss >> reg;
-            if (reg == 'A') A += 1;
-            else if (reg == 'B') B += 1;
-            else if (reg == 'C') C += 1;
-            else if (reg == 'D') D += 1;
-        } else if (opcode == 63) {
+            getRegister(reg) += 1;
+        } 
+        else if (opcode == 63) { // exit on error
             std::cerr << "division by 0" << std::endl;
             return A;
         }
@@ -67,19 +54,19 @@ int32_t runVM()
 void fibonacciProgram(unsigned n)
 {
     std::vector<std::string> program;
-    program.push_back("10A0"); // Set f0 to 0
-    program.push_back("10B1"); // Set f1 to 1
-    program.push_back("10C0"); // Set fn to 0
+    program.push_back("10 A 0"); // Set A = 0 (f0)
+    program.push_back("10 B 1"); // Set B = 1 (f1)
+    program.push_back("10 C 0"); // Set C = 0 (fn)
 
     for (unsigned i = 2; i <= n; i++) {
-        program.push_back("20D A"); // D = A (fn-2)
-        program.push_back("20A B"); // A = B (fn-1)
-        program.push_back("20B C"); // B = C (fn)
-        program.push_back("50C");   // C = A + 1 (fn+1)
+        program.push_back("20 D A"); // D = A (fn-2)
+        program.push_back("20 A B"); // A = B (fn-1)
+        program.push_back("20 B C"); // B = C (fn)
+        program.push_back("50 C");   // C = C + 1 (fn+1)
     }
 
-    for (const std::string& instruction : program) {
-        std::cout << instruction << std::endl;
+    for (const auto& instr : program) {
+        std::cout << instr << std::endl;
     }
 }
 
